b3b8e1b087e8a55517aa436b2d5c99383fc3282e
rexsimiloluwah <adetoyosi2001@gmail.com>
2023-11-01 03:20:14 +0100

    Testing webhook integration with Jenkins

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World. From Jenkins (Updated)')
+    res.send('Hello World. From Jenkins (Updated Again)')
 })
 
 app.get('/healthcheck', (req, res) => {
